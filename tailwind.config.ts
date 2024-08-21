b09fcb179c1f699105f9e476f35bb2009f57c274
CodeforNitin <codeforni3@gmail.com>
2024-08-22 00:57:01 +0530

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,81 @@
 import type { Config } from "tailwindcss";
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+      colors: {
+        main: "#88aaee",
+        mainAccent: "#58a769", // not needed for shadcn components
+        overlay: "rgba(0,0,0,0.8)", // background color overlay for alert dialogs, modals, etc.
+
+        // light mode
+        bg: "#daf5f0",
+        text: "#000",
+        border: "#000",
+
+        // dark mode
+        darkBg: "#0b2823",
+        darkText: "#eeefe9",
+        darkBorder: "#000",
+        secondaryBlack: "#1b1b1b", // opposite of plain white, not used pitch black because borders and box-shadows are that color
+
+        blue: {
+          100: "#B4C6EE",
+          400: "#417BFF",
+          500: "#3371FF",
+        },
+        red: {
+          400: "#DD4F56",
+          500: "#DC4349",
+        },
+        dark: {
+          100: "#09111F",
+          200: "#0B1527",
+          300: "#0F1C34",
+          350: "#12213B",
+          400: "#27344D",
+          500: "#2E3D5B",
+        },
+      },
+      borderRadius: {
+        base: "8px",
+      },
+      boxShadow: {
+        light: "3px 4px 0px 0px #000",
+        dark: "3px 4px 0px 0px #000",
+      },
+      translate: {
+        boxShadowX: "3px",
+        boxShadowY: "4px",
+        reverseBoxShadowX: "-3px",
+        reverseBoxShadowY: "-4px",
+      },
+      fontWeight: {
+        base: "500",
+        heading: "700",
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;
