3e44b41d39d0a76cc41be0c4d8a9fc3874be7109
CodeforNitin <codeforni3@gmail.com>
2024-08-24 20:37:35 +0530

    Designing UI - Navbar

---
+++ components/design/Navbar.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import logoImage from "../../public/assets/images/logosaas.png";
+import MenuIcon from "../../public/assets/icons/menu.svg";
+
+const Navbar = () => {
+  return (
+    <div className="bg-black">
+    <div className="px-4 ">
+      <div className="py-4 flex items-center justify-between">
+        <div className="relative">
+          <div className="absolute w-full top-2 bottom-0 bg-[linear-gradient(to_right,#F87BFF,#FB29CF,#FFDD9B,#C2F0B1,#2FD8FE)] blur-md"></div>
+          <Image
+            src={logoImage}
+            alt="product image"
+            className="h-12 w-12 relative"
+          />
+        </div>
+        <div className="border border-white border-opacity-30 h-10 w-10 inline-flex justify-center items-center rounded-lg sm:hidden">
+          <MenuIcon className="text-white" />
+        </div>
+        <nav className="gap-6 items-center hidden sm:flex">
+            <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">About</a>
+            <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Features</a>
+            <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Update</a>
+            <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Help</a>
+            <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Customers</a>
+            <button className="bg-white py-2 px-4 rounded-lg font-bold">Try it for FREE!</button>
+        </nav>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Navbar;
